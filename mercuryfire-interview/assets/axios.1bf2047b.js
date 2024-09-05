0ce1599d41920b19cc1f4da8b4733b99cd9ddf04
willy <willy8742891@gmail.com>
2024-09-05 21:00:54 +0800

    feat: interview update.

---
+++ mercuryfire-interview/assets/axios.1bf2047b.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.44b29d28.js";import{a}from"./axios.6e1fcf85.js";const i=a.create({baseURL:"https://dahua.metcfire.com.tw/"});var s=t(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=i});export{i as api,s as default};
--- mercuryfire-interview/assets/axios.e8698ca8.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.89ce2b80.js";import{a}from"./axios.6e1fcf85.js";const i=a.create({baseURL:"https://dahua.metcfire.com.tw/"});var s=t(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=i});export{i as api,s as default};
